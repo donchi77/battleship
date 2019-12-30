74f8c935590b3d41d7d7ddffe20b894006756187
Simomiry <59335685+SimoMiry@users.noreply.github.com>
2019-12-30 17:53:46 +0100

    Add files via upload

--- Server.c
+++ Server.c
@@ -5,21 +5,24 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <stdbool.h> 
+#include <string.h> 
 
-#define SERVER_PORT 8080
+#define SERVER_PORT 8081
 
 bool check = true;
+char namePlayer1[32] = "", namePlayer2[32] = "";
 
-void player1(int ,int);
-void player2(int ,int);
+void action(int ,int);
+void getName(int ,int, int);
+//void getName1(int ,int);
+//void getName2(int ,int);
 
 int main (int argc, char **argv) {
-	int socketAttesa, client_len, socketComunica;
-	int asciLetto;                       
+	int socketC, client_len, socketD[2];                      
 	struct sockaddr_in server, client;
 	
 	printf ("socket()\n");
-	if((socketAttesa = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+	if((socketC = socket(AF_INET, SOCK_STREAM, 0)) == -1){
 		perror("chiamata della system call socket() fallita");
 		return(1);
 	}
@@ -29,65 +32,114 @@ int main (int argc, char **argv) {
 	server.sin_port = htons(SERVER_PORT);
 	
 	printf ("bind()\n");
-	if (bind(socketAttesa, (struct sockaddr*)&server, sizeof server) == -1){
+	if (bind(socketC, (struct sockaddr*)&server, sizeof server) == -1){
 		perror("chiamata della bind() fallita");
 		return(2);
 	}
 	
 	printf ("listen()\n");
-	listen(socketAttesa, 2);           
+	listen(socketC, 2);           
 
   	printf ("accept()\n");
-  	//for(int i = 0 ; i < 2 ; i++){
+  	for(int i = 0 ; i < 2 ; i++){
 	  client_len = sizeof(client);
-	  if ((socketComunica = accept(socketAttesa, (struct sockaddr*)&client, &client_len)) < 0)
+	  if ((socketD[i] = accept(socketC, (struct sockaddr*)&client, &client_len)) < 0)
 	  {
 	    perror("connessione non accettata");
 	    return(3);
 	  }
+	}
 
-	  char c = 'a';
-	  recv(socketComunica, &c, 1, 0);
-	  printf("char %c", c);
-	  send(socketComunica, &c, 1, 0);
+	getName(socketD[0], socketD[1], 1);
+	getName(socketD[1], socketD[0], 2);
+	printf("\n");
 
-	  close(socketComunica);
-}
-	/*}
+	printf("\nIl nome di p1 e': %s", namePlayer1);
+	printf("\nIl nome di p2 e': %s", namePlayer2);
 
-	
-	printf("KEKW");
-	
+	printf("\n");
+	printf("\n");
+
 	while(check){
-		printf("monkaS");
-		player1(socketComunica[0], socketComunica[1]);
-		player2(socketComunica[1], socketComunica[0]);
-		
+		action(socketD[0], socketD[1]);
+		action(socketD[1], socketD[0]);
+
 		//controllo caso di vittoria
 	}
 	
-	close(socketComunica[0]);
-	close(socketComunica[1]);
+	close(socketD[0]);
+	close(socketD[1]);
 }
 
-void player1(int in ,int out){
-	char p1;
+void action(int in ,int out){
+	char a;
 	
-	printf("monkaS_1");
-	
-  	if(recv(in, &p1, 1, 0) > 0){
-	    printf("\nil carattere ricevuto da p1 e': %c", p1);
-	    send(out, &p1, 1, 0);
+  	if(recv(in, &a, 1, 0) > 0){
+	    printf("\nil carattere ricevuto da p1 e': %c", a);
+	    send(out, &a, 1, 0);
   	}
 }
 
-void player2(int in ,int out){
-	char p2;
+void getName(int in ,int out, int saveName){
+	int nameLen;
+	char buffer[32] = "";
 	
-	printf("monkaS_2");
-	
-  	if(recv(in, &p2, 1, 0) > 0){
-	    printf("\nil carattere ricevuto da p2 e': %c ", p2);
-	    send(out, &p2, 1, 0);
+  	if(recv(in, &nameLen, 2, 0) > 0){
+	    printf("\nLunghezza nome di p%d e': %d",saveName, nameLen);
   	}
+	fflush(stdout);
+
+	if(recv(in, &buffer, nameLen, 0) > 0){
+		printf("\nIl buffer ora e': %s", buffer);
+  	}
+	fflush(stdout);
+
+	send(out, &nameLen, 2, 0);
+	fflush(stdout);
+	send(out, &buffer, nameLen, 0);
+	fflush(stdout);	
+
+	if(saveName == 1){
+		strcpy(namePlayer1, buffer);
+	} else {
+		strcpy(namePlayer2, buffer);
+	}
+}
+
+/*void getName1(int in ,int out){
+	int nameLen;
+	char buffer1[32];
+	
+  	if(recv(in, &nameLen, 2, 0) > 0){
+	    printf("\nLunghezza nome di p1 e': %d", nameLen);
+  	}
+
+	if(recv(in, &buffer1, nameLen, 0) > 0){
+	    printf("\nIl nome di p1 e': %s", buffer1);
+  	}
+
+	send(out, &nameLen, 2, 0);
+	send(out, &buffer1, nameLen, 0);
+
+	strcpy(namePlayer1, buffer1);
+	fflush(stdout);
+}
+
+void getName2(int in ,int out){
+	int nameLen;
+	char buffer2[32];
+	
+  	if(recv(in, &nameLen, 2, 0) > 0){
+	    printf("\nLunghezza nome di p2 e': %d", nameLen);
+  	}
+
+	if(recv(in, &buffer2, nameLen, 0) > 0){
+	    printf("\nIl nome di p2 e': %s", buffer2);
+  	}
+
+	send(out, &nameLen, 2, 0);
+	send(out, &buffer2, nameLen, 0);
+
+	strcpy(namePlayer2, buffer2);
+	fflush(stdout);
 }*/
